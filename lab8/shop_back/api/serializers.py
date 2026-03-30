4550d56d40b33383e1694f0350e8a9c9ad0f10f5
Dariya Yergaliyeva <dariya-yergaliyeva>
2026-03-30 16:17:39 +0500

    lab9

--- lab8/shop_back/api/serializers.py
+++ lab8/shop_back/api/serializers.py
@@ -5,12 +5,12 @@ from .models import Product, Category
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['id', 'name']
+        fields = '__all__'
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    category = CategorySerializer()
+    #category = CategorySerializer()
 
     class Meta:
         model = Product
-        fields = ['id', 'name', 'price', 'description', 'count', 'is_active', 'category']
+        fields = '__all__'
